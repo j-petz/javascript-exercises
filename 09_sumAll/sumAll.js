04af2c3ea29241ac2b8090f3c8e379e16fa06be4
jpetz <60827828+j-petz@users.noreply.github.com>
2025-11-18 09:24:48 +0100

    This commit is solving exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    let arr = [];
+    if (
+        start < 0 ||
+        end < 0 ||
+        !Number.isInteger(start) ||
+        !Number.isInteger(end)
+    ) {
+        return "ERROR";
+    } else if (start > end) {
+        for (let i = end; i <= start; i++) {
+            arr.push(i);
+        }
+    } else {
+        for (let i = start; i <= end; i++) {
+            arr.push(i);
+        }
+    }
+    let result = arr.reduce(
+        (accumulator, currentValue) => accumulator + currentValue
+    );
+    return result;
 };
 
 // Do not edit below this line
