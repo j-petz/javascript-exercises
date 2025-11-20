aa8684fc304941151ccfa5a97a188d596ade35c0
jpetz <60827828+j-petz@users.noreply.github.com>
2025-11-20 13:01:00 +0100

    Simplify function in exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,9 @@
 const convertToCelsius = function (val) {
-    let result = ((val - 32) * 5) / 9;
-    return Math.round(result * 10) / 10;
+    return Math.round((((val - 32) * 5) / 9) * 10) / 10;
 };
 
 const convertToFahrenheit = function (val) {
-    let result = (val * 9) / 5 + 32;
-    return Math.round(result * 10) / 10;
+    return Math.round(((val * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
